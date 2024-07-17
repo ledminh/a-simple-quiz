af357239c859a4ef11ff3ad281a85940e973b3e9
Minh Le <minh.le@ledminh.dev>
2024-07-17 17:51:31 -0400

    FinishScreen: Add dummy data

--- src/components/screens/FinishScreen.tsx
+++ src/components/screens/FinishScreen.tsx
@@ -1,7 +1,19 @@
 import React from "react";
 
 const FinishScreen = ({ onRestart }: FinishScreenProps) => {
-  return <div>FinishScreen</div>;
+  return (
+    <div>
+      <p>Thank you for taking the quiz!</p>
+      <p>Your score is: 4/4</p>
+      <h2>LeaderBoard</h2>
+      <ol>
+        <li>John Doe - 4/4</li>
+        <li>Jane Doe - 3/4</li>
+        <li>John Smith - 2/4</li>
+      </ol>
+      <button onClick={onRestart}>Restart</button>
+    </div>
+  );
 };
 
 export default FinishScreen;
