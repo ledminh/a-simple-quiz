7993d645d68f37fdad05863a3e57cf335d1a81ce
Minh Le <minh.le@ledminh.dev>
2024-07-17 19:13:40 -0400

    FinishScreen: Add styles

--- src/components/screens/FinishScreen.tsx
+++ src/components/screens/FinishScreen.tsx
@@ -2,18 +2,80 @@ import React from "react";
 
 const FinishScreen = ({ onRestart }: FinishScreenProps) => {
   return (
-    <div>
-      <p>Thank you for taking the quiz!</p>
-      <p>Your score is: 4/4</p>
-      <h2>LeaderBoard</h2>
-      <ol>
-        <li>John Doe - 4/4</li>
-        <li>Jane Doe - 3/4</li>
-        <li>John Smith - 2/4</li>
-      </ol>
-      <button onClick={onRestart}>Restart</button>
+    <div className="flex flex-col gap-3">
+      <div>
+        <p>Thank you for taking the quiz!</p>
+        <p>Your score is: 4/4</p>
+      </div>
+
+      <div className="flex flex-col gap-2 bg-gray-300 p-4 rounded-md">
+        <h2>Leader Board</h2>
+        <ol>
+          {LeaderBoardData.map((data, i) => (
+            <li key={data.id}>
+              {i + 1} - {data.name} - {data.score}%
+            </li>
+          ))}
+        </ol>
+      </div>
+      <div>
+        <button onClick={onRestart}>Restart</button>
+      </div>
     </div>
   );
 };
 
 export default FinishScreen;
+
+const LeaderBoardData = [
+  {
+    id: 1,
+    name: "John Doe",
+    score: 100,
+  },
+  {
+    id: 2,
+    name: "Jane Doe",
+    score: 75,
+  },
+  {
+    id: 3,
+    name: "John Smith",
+    score: 50,
+  },
+  {
+    id: 4,
+    name: "John Doe",
+    score: 25,
+  },
+  {
+    id: 5,
+    name: "Jane Doe",
+    score: 50,
+  },
+  {
+    id: 6,
+    name: "John Smith",
+    score: 75,
+  },
+  {
+    id: 7,
+    name: "John Doe",
+    score: 100,
+  },
+  {
+    id: 8,
+    name: "Jane Doe",
+    score: 100,
+  },
+  {
+    id: 9,
+    name: "John Smith",
+    score: 100,
+  },
+  {
+    id: 10,
+    name: "John Smith",
+    score: 100,
+  },
+];
