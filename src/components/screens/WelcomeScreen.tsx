76fd0d01c33da1dec3f5f7f4392e39d2126c5489
Minh Le <minh.le@ledminh.dev>
2024-07-17 22:02:50 -0400

    WelcomeScreen: Handle Error

--- src/components/screens/WelcomeScreen.tsx
+++ src/components/screens/WelcomeScreen.tsx
@@ -1,6 +1,19 @@
 import React from "react";
 
 const WelcomeScreen = ({ onStart, name, setName }: WelcomeScreenProps) => {
+  const [error, setError] = React.useState<boolean>(false);
+
+  const onClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+
+    if (name.length === 0) {
+      setError(true);
+    } else {
+      setError(false);
+      onStart();
+    }
+  };
+
   return (
     <form className="flex flex-col gap-4">
       <h2>Welcome to Simple Quiz</h2>
@@ -16,8 +29,9 @@ const WelcomeScreen = ({ onStart, name, setName }: WelcomeScreenProps) => {
             onChange={(e) => setName(e.target.value)}
             className="border border-gray-600 p-2 rounded-md"
           />
-          <button onClick={onStart}>Begin</button>
+          <button onClick={onClick}>Begin</button>
         </div>
+        {error && <p className="text-red-600">Please enter your name</p>}
       </div>
     </form>
   );
